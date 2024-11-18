1f8c2b34a9847484f274498f10510b0b975cec90
zeusho <zeus@smallho.cn>
2024-11-18 22:37:01 +0800

    feat: 部署项目

--- main.py
+++ main.py
@@ -85,7 +85,7 @@ class MainProcess(object):
 
             # 使用 with 语句创建一个进度条
             with alive_bar(force_tty=True) as bar: # 给 alive_bar 传入进度条总数目（这里是 100）
-                for item in range(500):
+                for item in range(100):
                     # 等待 1s
                     time.sleep(0.1)
                     #更新进度条，进度 +1
